258f235cb52c3df4d6269e4fd23568a86e4cc657
thao <mrtrados@gmail.com>
2024-08-09 19:11:52 +0900

    Add FormInputZipcode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// import DocumentScan from '../actions/Document-scan';
+import FormInputZipcode from '@/components/FormInputZipcode';
 import { CameraCapture } from './../components/CameraCapture';
 import { callJpPostApi } from "@/actions/JpPostApi";
 
@@ -6,20 +6,12 @@ import { callJpPostApi } from "@/actions/JpPostApi";
 export const runtime = 'edge';
 
 export default function Home() {
-  callJpPostApi("5320002");
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            href="D:\Projects\tokyo-hackathon-ai-2024\src\app\libs\Document-scan.tsx"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          ></a>
-          <CameraCapture />
 
-        </div>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <FormInputZipcode />
+        <CameraCapture />
       </div>
     </main>
   );
