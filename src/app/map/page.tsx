5b3426beed7e4d289e1118683581372a0a84cfe3
thao <mrtrados@gmail.com>
2024-08-16 09:34:07 +0900

    UPDATE page with DrawOnMap

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,14 +1,12 @@
-import TrashScheduler from "@/components/TrashScheduler";
-import Image from "next/image";
-// import DocumentScan from '../actions/Document-scan';
+import DrawOnMap from "@/components/DrawOnMap";
 
 
 export const runtime = 'edge';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+    <main className="flex min-h-screen flex-col items-center justify-between px-8 pt-8">
+      <DrawOnMap lat={36.1037824} long={136.2952192} />
     </main>
   );
 }
