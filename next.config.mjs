1236863e1b308a93a5b206580dc32b2b1801a04f
thao <mrtrados@gmail.com>
2024-08-16 09:29:47 +0900

    Add set up to get current location from cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// we import the utility from the next-dev submodule
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
+
+// we only need to use the utility during development so we can check NODE_ENV
+// (note: this check is recommended but completely optional)
+if (process.env.NODE_ENV === "development") {
+  // we simply need to call the utility
+  setupDevPlatform();
+}
