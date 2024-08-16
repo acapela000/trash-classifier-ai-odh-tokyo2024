5799db99ed07211860e43eff097cb6ff79211b11
thao <mrtrados@gmail.com>
2024-08-16 09:35:11 +0900

    UPDATE predictions, css, loadedmodel

--- src/components/ClassifierModels.tsx
+++ src/components/ClassifierModels.tsx
@@ -1,47 +1,71 @@
 'use client'
-import React, { useState, useEffect } from "react";
-// import json file
+import React, { useEffect } from "react";
 import * as tmImage from '@teachablemachine/image';
+import { CircularProgress } from '@nextui-org/react';
+
+
+type Props = {
+    img: any,
+}
 // More API functions here:
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+export default function ClassifierModels(props: Props) {
+    const [model, setModel] = React.useState<any>();
+    const [prediction, setPrediction] = React.useState<any>();
 
-export function ClassifierModels() {
-    // the link to your model provided by Teachable Machine export panel
-    const URL = "./tm-my-image-model";
-
-    const [model, setModel] = useState();
-    const [labelContainer, setLabelContainer] = useState();
-    const [maxPredictions, setMaxPredictions] = useState();
-
-    // Load the image model and setup the webcam
+    // Load the model when the component mounts
     useEffect(() => {
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
+        const runPrediction = async (m: any) => {
+            const pred = await m.predict(props.img)
 
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
-    }, []);
-
-
-    // run the webcam image through the image model
-    async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction =
-                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
+            setPrediction(pred)
         }
-    }
+
+        const loadModel = async () => {
+            const loadedModel: any = await tmImage.load('/tm-my-image-model/model.json', '/tm-my-image-model/metadata.json');
+            setModel(loadedModel);
+
+            runPrediction(loadedModel);
+        };
+
+        if (!model) {
+            loadModel();
+        } else {
+            runPrediction(model);
+        }
+    }, [props.img, model]);
 
     return (
-        <div>
-            <div id="webcam-container"></div>
-            <div id="label-container"></div>
+        <div className="flex gap-3">
+            {prediction && (
+                prediction.map((key: any, i: any) => (
+                    <div key={i} className="text-center">
+                        {key.className} <CircularProgress
+                            aria-label="Loading..."
+                            size="lg"
+                            value={key.probability * 100}
+                            color={
+                                (() => {
+                                    switch (key.className) {
+                                        case 'Can':
+                                            return 'warning';
+                                        case 'Carton':
+                                        case 'Paper':
+                                        case 'Petbottle':
+                                            return 'success';
+                                        case 'Glass':
+                                        case 'Plastic':
+                                            return 'danger';
+                                        default:
+                                            return 'secondary';
+                                    }
+                                })()
+                            }
+                            showValueLabel={true}
+                        />
+                    </div>
+                ))
+            )}
         </div>
     );
 }
