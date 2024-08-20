e07c554897c6aeff353820ca9439c99485610111
thao <mrtrados@gmail.com>
2024-08-20 23:36:38 +0900

    ADD arr of url for theme maps

--- src/components/DrawOnMap.tsx
+++ src/components/DrawOnMap.tsx
@@ -18,6 +18,27 @@ L.Icon.Default.mergeOptions({
 
 export default function MapComponent({ lat, long }: { lat: any, long: any }): any {
     const center: [number, number] = [lat, long];
+    const tileLayers = [
+        {
+            url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attribution: 'ArcGIS World Imagery',
+        },
+        {
+            url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            attribution: 'OpenStreetMap',
+        },
+        {
+            url: 'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/MODIS_Terra_Aerosol/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}',
+            attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+            bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+            minZoom: 1,
+            maxZoom: 6,
+            format: 'png',
+            time: '',
+            tilematrixset: 'GoogleMapsCompatible_Level',
+            opacity: 0.75,
+        }
+    ];
 
     return (
         <>
@@ -27,9 +48,38 @@ export default function MapComponent({ lat, long }: { lat: any, long: any }): an
                         Loren Sium
                     </Popup>
                 </Marker>
-                <TileLayer
-                    url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-                />
+                <Marker style="color: green" position={[36.5395968, 136.5835776]}>
+                    <Popup>
+                        Loren Sium
+                    </Popup>
+                </Marker>
+                {/* <TileLayer
+                    //url1='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+                    url1='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    url="https://map1.vis.earthdata.nasa.gov/wmts-webmerc/MODIS_Terra_Aerosol/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}"
+                    attribution='Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.'
+                    bounds={[[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]]}
+                    minZoom={1}
+                    maxZoom={6}
+                    format='png'
+                    time=''
+                    tilematrixset='GoogleMapsCompatible_Level'
+                    opacity={0.75}
+                /> */}
+                {tileLayers.map((layer, index) => (
+                    <TileLayer
+                        key={index}
+                        url={layer.url}
+                        attribution={layer.attribution}
+                        bounds={layer.bounds}
+                        minZoom={layer.minZoom}
+                        maxZoom={layer.maxZoom}
+                        format={layer.format}
+                        time={layer.time}
+                        tilematrixset={layer.tilematrixset}
+                        opacity={layer.opacity}
+                    />
+                ))}
             </MapContainer>
         </>
 
