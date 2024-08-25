748ac40ce7c4a14f931015f04cb8097731b5307c
thao <mrtrados@gmail.com>
2024-08-25 15:12:55 +0900

    ADD MapWrapper

---
+++ src/components/MapWrapper.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React, { useState } from 'react';
+import MapComponent from "@/components/DrawOnMap";
+import FormInputZipcode from "@/components/FormInputZipcode";
+
+interface Props {
+    cf: any,
+}
+
+export default function MapWrapper({ cf }: Props) {
+    const [location, setLocation] = useState<{ latitude: number, longitude: number }>({ latitude: 0, longitude: 0 });
+
+    return (
+        <>
+            <FormInputZipcode cf={cf} setLocation={setLocation} location={location} />
+            <MapComponent lat={location.latitude} long={location.longitude} />
+        </>
+    )
+}
