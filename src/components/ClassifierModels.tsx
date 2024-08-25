202d3860822b4af018136e91e77ab7628d043397
thao <mrtrados@gmail.com>
2024-08-25 15:11:14 +0900

    UPDATE prediction result for more accurate

--- src/components/ClassifierModels.tsx
+++ src/components/ClassifierModels.tsx
@@ -1,24 +1,30 @@
 'use client'
 import React, { useEffect } from "react";
 import * as tmImage from '@teachablemachine/image';
-import { CircularProgress } from '@nextui-org/react';
+import { CircularProgress, Spinner } from '@nextui-org/react';
 
 
 type Props = {
-    img: any,
+    // img: any,
+    imgSrc: string,
 }
 // More API functions here:
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 export default function ClassifierModels(props: Props) {
+    // const img = props.img;
     const [model, setModel] = React.useState<any>();
     const [prediction, setPrediction] = React.useState<any>();
 
     // Load the model when the component mounts
     useEffect(() => {
-        const runPrediction = async (m: any) => {
-            const pred = await m.predict(props.img)
-
-            setPrediction(pred)
+        function runPrediction(m: any) {
+            setPrediction(undefined)
+            const img = new Image();
+            img.onload = async () => {
+                const pred = await m.predict(img)
+                setPrediction(pred);
+            }
+            img.src = props.imgSrc;
         }
 
         const loadModel = async () => {
@@ -33,39 +39,49 @@ export default function ClassifierModels(props: Props) {
         } else {
             runPrediction(model);
         }
-    }, [props.img, model]);
+    }, [props.imgSrc, model]);
 
     return (
-        <div className="flex gap-3">
-            {prediction && (
-                prediction.map((key: any, i: any) => (
-                    <div key={i} className="text-center">
-                        {key.className} <CircularProgress
-                            aria-label="Loading..."
-                            size="lg"
-                            value={key.probability * 100}
-                            color={
-                                (() => {
-                                    switch (key.className) {
-                                        case 'Can':
-                                            return 'warning';
-                                        case 'Carton':
-                                        case 'Paper':
-                                        case 'Petbottle':
-                                            return 'success';
-                                        case 'Glass':
-                                        case 'Plastic':
-                                            return 'danger';
-                                        default:
-                                            return 'secondary';
-                                    }
-                                })()
-                            }
-                            showValueLabel={true}
-                        />
-                    </div>
-                ))
-            )}
-        </div>
+        <section className="w-full bg-white p-2 rounded-lg shadow-md mt-4">
+            <h3 className="text-xl font-bold">Prediction Result</h3>
+            <section className="mt-4 grid grid-cols-3 gap-3 items-center justify-center">
+                {prediction ? (
+                    prediction.map((key: any, i: any) => (
+                        <div key={i} className="text-center">
+                            {key.className} <CircularProgress
+                                aria-label="Loading..."
+                                size="lg"
+                                classNames={{
+                                    svg: "w-24 h-24",
+                                    value: "text-2xl font-semibold",
+                                }}
+                                value={key.probability * 100}
+                                color={
+                                    (() => {
+                                        switch (key.className) {
+                                            case 'Can':
+                                                return 'warning';
+                                            case 'Carton':
+                                            case 'Paper':
+                                            case 'Petbottle':
+                                                return 'success';
+                                            case 'Glass':
+                                            case 'Plastic':
+                                                return 'danger';
+                                            // case 'Raw trash':
+                                            //     return 'primary';
+                                            default:
+                                                return 'secondary';
+                                        }
+                                    })()
+                                }
+                                showValueLabel={true}
+                            />
+                        </div>
+                    ))
+                ) : <Spinner color="success" size="lg" />}
+            </section>
+
+        </section>
     );
 }
