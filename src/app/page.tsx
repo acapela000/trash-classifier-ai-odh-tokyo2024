f093b83a97c58b65d6075bf1879f96d6c1e6d995
thao <mrtrados@gmail.com>
2024-08-25 15:07:54 +0900

    UPDATE Home with RecyclingHints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
-import FormInputZipcode from '@/components/FormInputZipcode';
 import { CameraCapture } from '@/components/CameraCapture';
-
-import { getRequestContext } from '@cloudflare/next-on-pages';
+import RecyclingHints from '@/components/RecyclingHints';
 
 export const runtime = 'edge';
 
 export default async function Home() {
 
-  const { cf } = getRequestContext();
-
   return (
-    <main className="flex flex-col items-center w-full min-h-screen bg-gray-100">
-
-
+    <main className="flex flex-col items-center w-full min-h-screen">
       <div className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <FormInputZipcode cf={cf} />
-          <CameraCapture />
+        <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+          <section className='min-h-[100vh]'>
+            <CameraCapture />
+          </section>
+          <RecyclingHints hints={[]} />
         </div>
       </div>
     </main>
