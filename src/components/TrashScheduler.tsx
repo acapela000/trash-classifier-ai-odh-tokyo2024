56ae50f622496979c729d7eabe33025b5ec761b4
thao <mrtrados@gmail.com>
2024-08-25 15:14:39 +0900

    UPDATE with echart

--- src/components/TrashScheduler.tsx
+++ src/components/TrashScheduler.tsx
@@ -1,99 +1,54 @@
 "use client";
 import React from "react";
-import { Calendar, Radio, RadioGroup, Button, ButtonGroup, cn } from "@nextui-org/react";
-import { today, getLocalTimeZone, startOfWeek, startOfMonth } from "@internationalized/date";
-import { useLocale } from "@react-aria/i18n";
+import ReactEChartsCore from 'echarts-for-react/lib/core';
+import * as echarts from 'echarts/core';
+import { CustomChart } from 'echarts/charts';
+import { CalendarComponent, TooltipComponent } from 'echarts/components';
+import { SVGRenderer } from 'echarts/renderers';
+import { range } from "@tensorflow/tfjs";
 
-// export default function TrashScheduler() {
-//     let [value, setValue] = React.useState(parseDate("2024-10-08"));
-
-//     return (
-//             <Calendar aria-label="Date (International Calendar)"
-//                 value={today(getLocalTimeZone())}
-//             />
-//     );
-// }
+echarts.use(
+    [CustomChart, CalendarComponent, SVGRenderer, TooltipComponent]
+);
+const config = {
+    tooltip: {},
+    calendar: [
+        {
+            left: 'center',
+            top: 'middle',
+            cellSize: [70, 70],
+            yearLabel: { show: false },
+            orient: 'vertical',
+            dayLabel: {
+                firstDay: 1,
+            },
+            monthLabel: {
+                show: false
+            },
+            range: '2024-07'
+        }
+    ],
+    series: {
+        type: 'custom',
+        coordinateSystem: 'calendar',
+        dimensions: [undefined, { type: 'ordinal' }],
+        data: [],
+        renderItem: function (params: any, api: any) { return; }
+    }
+};
 
 export default function TrashScheduler() {
-    let defaultDate = today(getLocalTimeZone());
-    let [value, setValue] = React.useState(defaultDate);
-    let { locale } = useLocale();
-
-    let now = today(getLocalTimeZone());
-    let nextWeek = startOfWeek(now.add({ weeks: 1 }), locale);
-    let nextMonth = startOfMonth(now.add({ months: 1 }));
-
-    const CustomRadio = (props: any) => {
-        const { children, ...otherProps } = props;
-
-        return (
-            <Radio
-                {...otherProps}
-                classNames={{
-                    base: cn(
-                        "flex-none m-0 h-8 bg-content1 hover:bg-content2 items-center justify-between",
-                        "cursor-pointer rounded-full border-2 border-default-200/60",
-                        "data-[selected=true]:border-primary",
-                    ),
-                    label: "text-tiny text-default-500",
-                    labelWrapper: "px-1 m-0",
-                    wrapper: "hidden",
-                }}
-            >
-                {children}
-            </Radio>
-        );
-    };
+    const [option, setOption] = React.useState(config);
+    //get current year/month
+    // const now = (new Date().getMonth() + 1).toString() + '-' + new Date().getFullYear().toString();
+    // range = now;
 
     return (
-        <div className="flex flex-col gap-4">
-            <Calendar
-                aria-label="Date (Presets)"
-                bottomContent={
-                    <RadioGroup
-                        aria-label="Date precision"
-                        classNames={{
-                            base: "w-full pb-2",
-                            wrapper: "-my-2.5 py-2.5 px-3 gap-1 flex-nowrap max-w-[280px] overflow-x-scroll",
-                        }}
-                        defaultValue="exact_dates"
-                        orientation="horizontal"
-                    >
-                        <CustomRadio value="exact_dates">Exact dates</CustomRadio>
-                        <CustomRadio value="1_day">1 day</CustomRadio>
-                        <CustomRadio value="2_days">2 days</CustomRadio>
-                        <CustomRadio value="3_days">3 days</CustomRadio>
-                        <CustomRadio value="7_days">7 days</CustomRadio>
-                        <CustomRadio value="14_days">14 days</CustomRadio>
-                    </RadioGroup>
-                }
-                classNames={{
-                    content: "w-full",
-                }}
-                focusedValue={value}
-                nextButtonProps={{
-                    variant: "bordered",
-                }}
-                prevButtonProps={{
-                    variant: "bordered",
-                }}
-                topContent={
-                    <ButtonGroup
-                        fullWidth
-                        className="px-3 pb-2 pt-3 bg-content1 [&>button]:text-default-500 [&>button]:border-default-200/60"
-                        radius="full"
-                        size="sm"
-                        variant="bordered"
-                    >
-                        <Button onPress={() => setValue(now)}>Today</Button>
-                        <Button onPress={() => setValue(nextWeek)}>Next week</Button>
-                        <Button onPress={() => setValue(nextMonth)}>Next month</Button>
-                    </ButtonGroup>
-                }
-                value={value}
-                onChange={setValue}
-                onFocusChange={setValue}
-            />
-        </div>
+        <ReactEChartsCore
+            echarts={echarts}
+            option={option}
+            lazyUpdate={true}
+            style={{ width: "100%", height: "90vh" }}
+        />
     );
 }
