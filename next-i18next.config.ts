f8e2716b314363a4501de3eb37beb1774f084e0c
thao <mrtrados@gmail.com>
2024-08-29 23:46:42 +0900

    REMOVE nexti18n

--- next-i18next.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-i18next').UserConfig} */
-module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pt', 'ru', 'zh', 'vi', 'id', 'ne', 'tl', 'hi'],
-    },
-  }
