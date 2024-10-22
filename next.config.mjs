532207079fa8271704bf34762db0f70f19f33252
thao <mrtrados@gmail.com>
2024-10-22 14:47:39 +0900

    UPDATE prettier/eslint config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 // we import the utility from the next-dev submodule
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-import createNextIntlPlugin from "next-intl/plugin";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 // we only need to use the utility during development so we can check NODE_ENV
 // (note: this check is recommended but completely optional)
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   // we simply need to call the utility
   setupDevPlatform();
 }
@@ -12,8 +12,6 @@ if (process.env.NODE_ENV === "development") {
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    exclude: ['migrations', 'drizzle.config.ts', 'ai'],
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
